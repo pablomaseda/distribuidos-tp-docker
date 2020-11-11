8568742264e1b9344685d4b03316ec09753d2353
pablomaseda <pablomaseda@gmail.com>
2020-11-10 22:48:02 -0300

    Update deploy-enviroment.sh
    MVN tomar ruta con which para correr en otros S.O.

--- deploy-enviroment.sh
+++ deploy-enviroment.sh
@@ -3,6 +3,8 @@
 # Volumen para persistencia
 mkdir mysqlDB
 
+MVN=$(which mvn)
+
 # Configuro el frontend
 
 git clone https://github.com/gonzaberro/frontDistribuidosReact.git reactApp
@@ -13,20 +15,20 @@ cp Dockerfile.reactApp reactApp/Dockerfile
 git clone  -b development --single-branch https://github.com/Gianni-Nicolas/modulo-administrador.git adminApp
 cp Dockerfile.springApp adminApp/Dockerfile 
 cd adminApp
-/usr/bin/mvn package
+$MVN package
 
 cd ..
 git clone  -b development --single-branch https://github.com/Gianni-Nicolas/modulo-docente.git docenteApp
 cp Dockerfile.springApp docenteApp/Dockerfile 
 cd docenteApp
-/usr/bin/mvn package
+$MVN package
 
 cd ..
 
 git clone  -b development --single-branch https://github.com/Gianni-Nicolas/modulo-estudiante.git estudianteApp
 cp Dockerfile.springApp estudianteApp/Dockerfile 
 cd estudianteApp
-/usr/bin/mvn package
+$MVN package
 cd ..
 
 
@@ -34,6 +36,5 @@ cd ..
 #docker-compose build && docker-compose up
 
 #cat init-data.sql | docker exec -i CONTAINER /usr/bin/mysql -u root --password=12345678 TrabajoFinal
-
 
 
