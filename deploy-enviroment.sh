190a31dc5a2e95bbb313b76d5a1a0a78ae97bf4f
pablomaseda <pablomaseda@gmail.com>
2020-11-10 21:56:49 -0300

    Update deploy-enviroment.sh

--- deploy-enviroment.sh
+++ deploy-enviroment.sh
@@ -10,28 +10,30 @@ cp Dockerfile.reactApp reactApp/Dockerfile
 
 # Configuro servicios del backend
 
-git clone https://github.com/Gianni-Nicolas/modulo-administrador.git adminApp
+git clone  -b development --single-branch https://github.com/Gianni-Nicolas/modulo-administrador.git adminApp
 cp Dockerfile.springApp adminApp/Dockerfile 
 cd adminApp
-./mvn package
+/usr/bin/mvn package
 
-git clone https://github.com/Gianni-Nicolas/modulo-docente.git docenteApp
+cd ..
+git clone  -b development --single-branch https://github.com/Gianni-Nicolas/modulo-docente.git docenteApp
 cp Dockerfile.springApp docenteApp/Dockerfile 
 cd docenteApp
-./mvn package
+/usr/bin/mvn package
 
-git clone https://github.com/Gianni-Nicolas/modulo-estudiante.git estudianteApp
+cd ..
+
+git clone  -b development --single-branch https://github.com/Gianni-Nicolas/modulo-estudiante.git estudianteApp
 cp Dockerfile.springApp estudianteApp/Dockerfile 
 cd estudianteApp
-./mvn package
-
+/usr/bin/mvn package
+cd ..
 
 
 # Al final ejecutar
 #docker-compose build && docker-compose up
 
 #cat init-data.sql | docker exec -i CONTAINER /usr/bin/mysql -u root --password=12345678 TrabajoFinal
-
 
 
 
